8277317695c090f2f9776ca2567c932f9dfd322b
suyu0925 <suyu0925@gmail.com>
2017-11-19 16:43:53 +0800

    feedback tv script generation project

--- tv-script-generation/dlnd_tv_script_generation.py
+++ tv-script-generation/dlnd_tv_script_generation.py
@@ -232,7 +232,7 @@ tests.test_get_inputs(get_inputs)
 # 
 # Return the cell and initial state in the following tuple `(Cell, InitialState)`
 
-# In[23]:
+# In[9]:
 
 
 def get_init_cell(batch_size, rnn_size):
@@ -290,7 +290,7 @@ tests.test_get_embed(get_embed)
 # 
 # Return the outputs and final_state state in the following tuple `(Outputs, FinalState)` 
 
-# In[13]:
+# In[11]:
 
 
 def build_rnn(cell, inputs):
@@ -320,7 +320,7 @@ tests.test_build_rnn(build_rnn)
 # 
 # Return the logits and final state in the following tuple (Logits, FinalState) 
 
-# In[14]:
+# In[12]:
 
 
 def build_nn(cell, rnn_size, input_data, vocab_size, embed_dim):
@@ -374,10 +374,10 @@ tests.test_build_nn(build_nn)
 # ]
 # ```
 
-# In[20]:
+# In[16]:
 
 
-def get_batches(int_text, batch_size, seq_length):
+def bad_get_batches(int_text, batch_size, seq_length):
     """
     Return batches of input and target
     :param int_text: Text with the words replaced by their ids
@@ -404,8 +404,23 @@ def get_batches(int_text, batch_size, seq_length):
         y = np.zeros_like(x)
         y[:, :-1], y[:, -1] = x[:, 1:], x[:, 0]
         batches.append([x, y])
-        
+
     return np.array(batches)
+
+def get_batches(int_text, batch_size, seq_length):
+    # Another implemention
+    n_batches = int(len(int_text) / (batch_size * seq_length))
+    # Drop the last few characters to make only full batches
+    xdata = np.array(int_text[: n_batches * batch_size * seq_length])
+    ydata = np.array(int_text[1: n_batches * batch_size * seq_length + 1])
+    ydata[-1] = xdata[0]
+
+    x_batches = np.split(xdata.reshape(batch_size, -1), n_batches, 1)
+    y_batches = np.split(ydata.reshape(batch_size, -1), n_batches, 1)
+    return np.array(list(zip(x_batches, y_batches)))
+
+# print(bad_get_batches([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], 2, 3))
+# print(get_batches([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], 2, 3))
 
 
 """
@@ -426,11 +441,11 @@ tests.test_get_batches(get_batches)
 # - Set `learning_rate` to the learning rate.
 # - Set `show_every_n_batches` to the number of batches the neural network should print progress.
 
-# In[26]:
+# In[17]:
 
 
 # Number of Epochs
-num_epochs = 100
+num_epochs = 200
 # Batch Size
 batch_size = 100
 # RNN Size
@@ -440,9 +455,9 @@ embed_dim = 256
 # Sequence Length
 seq_length = 30
 # Learning Rate
-learning_rate = 0.001
+learning_rate = 0.003
 # Show stats for every n number of batches
-show_every_n_batches = 69
+show_every_n_batches = 46
 
 """
 DON'T MODIFY ANYTHING IN THIS CELL THAT IS BELOW THIS LINE
@@ -453,7 +468,7 @@ save_dir = './save'
 # ### Build the Graph
 # Build the graph using the neural network you implemented.
 
-# In[27]:
+# In[18]:
 
 
 """
@@ -490,7 +505,7 @@ with train_graph.as_default():
 # ## Train
 # Train the neural network on the preprocessed data.  If you have a hard time getting a good loss, check the [forms](https://discussions.udacity.com/) to see if anyone is having the same problem.
 
-# In[28]:
+# In[19]:
 
 
 """
@@ -529,7 +544,7 @@ with tf.Session(graph=train_graph) as sess:
 # ## Save Parameters
 # Save `seq_length` and `save_dir` for generating a new TV script.
 
-# In[29]:
+# In[20]:
 
 
 """
@@ -541,7 +556,7 @@ helper.save_params((seq_length, save_dir))
 
 # # Checkpoint
 
-# In[30]:
+# In[21]:
 
 
 """
@@ -566,7 +581,7 @@ seq_length, load_dir = helper.load_params()
 # 
 # Return the tensors in the following tuple `(InputTensor, InitialStateTensor, FinalStateTensor, ProbsTensor)` 
 
-# In[31]:
+# In[22]:
 
 
 def get_tensors(loaded_graph):
@@ -592,7 +607,7 @@ tests.test_get_tensors(get_tensors)
 # ### Choose Word
 # Implement the `pick_word()` function to select the next word using `probabilities`.
 
-# In[32]:
+# In[23]:
 
 
 def pick_word(probabilities, int_to_vocab):
@@ -615,7 +630,7 @@ tests.test_pick_word(pick_word)
 # ## Generate TV Script
 # This will generate the TV script for you.  Set `gen_length` to the length of TV script you want to generate.
 
-# In[33]:
+# In[24]:
 
 
 gen_length = 200
